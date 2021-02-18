4094ed831493c7b01873499a88a733e046a0a8ee
William Yao <wmyaoyao@gmail.com>
2021-02-18 17:17:57 +0800

    remove unused function

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from news_classifier import biznews
 
 news_text = "this is a news...."
 
-c = biznews.init()
-result = biznews.classify(news_text, c)
+nreader = biznews.init()
+result = nreader.classify(news_text)
 
 print(result)
--- news_classifier/biznews.py
+++ news_classifier/biznews.py
@@ -1,13 +1,14 @@
 # import json
 
 def init():
-    """ init ..."""
+    """ Create an object and init it. Make it as a function as we may need to config something."""
     c = NewsReader()
-    return c
 
-def classify(news, c):
-    """ Classify news """
-    return c.classify(news)
+    # May need to assign/setup the class, e.g.,
+    MODEL_PATH = r'data/model'
+    c.model = MODEL_PATH
+    
+    return c
 
 class NewsReader():
     """ News Classifier for Business related news."""
@@ -16,6 +17,18 @@ class NewsReader():
         pass
 
     def classify(self, news_text):
+        """Gets and prints the spreadsheet's header columns
+
+        Parameters
+        ----------
+        nets_text : str
+            The target news content as a string.
+
+        Returns
+        -------
+        dict
+            News classify result. 
+        """
 
         result = {
             'NN': True,
