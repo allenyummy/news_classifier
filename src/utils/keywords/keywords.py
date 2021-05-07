bca7b0153735a988db469dc62188d342adf47232
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-05-07 11:27:42 +0800

    [add] BaseKeywordsLoader

--- src/utils/keywords/keywords.py
+++ src/utils/keywords/keywords.py
@@ -5,8 +5,7 @@
 import logging
 import os
 from typing import List, Tuple, Union, Optional
-from abc import ABC, abstractmethod
-from src.utils import utility as ut
+from src.utils.keywords.base import BaseKeywordsLoader
 
 logger = logging.getLogger(__name__)
 
@@ -24,50 +23,13 @@ def KeywordsFactory(
 ):
 
     LOCALIZERS = {
-        "Negative_News": NegativeNewsKeywords,
-        "ESG_News": ESGNewsKeywords,
+        "Negative_News": NegativeNewsKeywordsLoader,
+        "ESG_News": ESGNewsKeywordsLoader,
     }
     return LOCALIZERS[name](keywords, load_default)
 
 
-class Keywords(ABC):
-
-    DEFAULT_DIR = None
-
-    def load(
-        self,
-        keywords: Optional[Union[str, List[str]]] = None,
-        load_default: Optional[bool] = True,
-    ) -> List[str]:
-
-        ret = list()
-        ret.extend(ut.load(keywords))
-
-        if load_default:
-            ret.extend(
-                ut.load(
-                    [
-                        os.path.join(self.DEFAULT_DIR, file)
-                        for file in os.listdir(self.DEFAULT_DIR)
-                        if file.endswith(".txt")
-                    ]
-                )
-            )
-        return ret
-
-    @property
-    @abstractmethod
-    def keywords(self) -> Tuple[str]:
-        ## It's not allowed to change self.keywords.
-        ## Property decorator makes it impossible to set attribute.
-        ## It means we can't assign values to self.keywords. (e.g., self.keywords = XXX)
-        ## However, it still can do operations of "append", "remove", "add" and so on.
-        ## If type of self.keywords is list or set, we can modify it (e.g., self.keywords.append(XXX)), which shouldn't be allowed.
-        ## So, we need to return self.keywords as tuple of string whose feature is that we can't modify elements in self.keywords.
-        raise NotImplemented
-
-
-class NegativeNewsKeywords(Keywords):
+class NegativeNewsKeywordsLoader(BaseKeywordsLoader):
 
     DEFAULT_DIR = DEFAULT_DIR_PATH["Negative_News"]
 
@@ -76,14 +38,14 @@ class NegativeNewsKeywords(Keywords):
         keywords: Optional[Union[str, List[str]]] = None,
         load_default: Optional[bool] = True,
     ):
-        self._keywords = self.load(keywords, load_default)
+        super().__init__(keywords, load_default)
 
     @property
     def keywords(self) -> Tuple[str]:
-        return tuple(self._keywords)
+        return super().keywords
 
 
-class ESGNewsKeywords(Keywords):
+class ESGNewsKeywordsLoader(BaseKeywordsLoader):
 
     DEFAULT_DIR = DEFAULT_DIR_PATH["ESG_News"]
 
@@ -92,8 +54,17 @@ class ESGNewsKeywords(Keywords):
         keywords: Optional[Union[str, List[str]]] = None,
         load_default: Optional[bool] = True,
     ):
-        self._keywords = self.load(keywords, load_default)
+        super().__init__(keywords, load_default)
 
     @property
     def keywords(self) -> Tuple[str]:
-        return tuple(self._keywords)
+        return super().keywords
+
+
+if __name__ == "__main__":
+
+    nn = NegativeNewsKeywordsLoader(keywords="我")
+    print(nn.keywords)
+
+    esg = ESGNewsKeywordsLoader()
+    print(esg.keywords)
