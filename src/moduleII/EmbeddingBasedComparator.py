14c779aac9aa8778967b896f488cc23f512ff4d5
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-05-10 14:37:57 +0800

    [add] _evaluate_for_keywords_for_document

--- src/moduleII/EmbeddingBasedComparator.py
+++ src/moduleII/EmbeddingBasedComparator.py
@@ -15,8 +15,10 @@ from flair.embeddings import (
 from src.utils import struct as st, utility as ut, evaluation as ev
 from src.utils.keywords import keywords as ke
 from src.moduleII.base import BaseComparator
+from src.keyExtractor import keyExtractor
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class EmbeddingBasedComparator(BaseComparator):
@@ -54,10 +56,11 @@ class EmbeddingBasedComparator(BaseComparator):
         news_body: str,
         threshold: Optional[float] = 0.6,  # -1.0 <= threshold <= 1.0
         top_n: Optional[int] = 5,
+        method: Optional[str] = "doc_based",  # doc_based or key_based
     ):
-        document = f"{news_title}。{news_body}"
+
         news_category, score, most_likely_keywords = self.evaluate(
-            document, threshold, top_n
+            news_title, news_body, threshold, top_n, method
         )
 
         ret = st.RetStruct(
@@ -70,11 +73,23 @@ class EmbeddingBasedComparator(BaseComparator):
 
     def evaluate(
         self,
-        document: str,
+        news_title: str,
+        news_body: str,
         threshold: Optional[float] = 0.6,  # -1.0 <= threshold <= 1.0
         top_n: Optional[int] = 5,
+        method: Optional[str] = "doc_based",  # doc_based or key_based
     ):
-        return self._evaluate_for_document(document, threshold, top_n)
+
+        if method == "doc_based":
+            document = news_title
+            return self._evaluate_for_document(document, threshold, top_n)
+        elif method == "key_based":
+            document = f"{news_title}。{news_body}"
+            return self._evaluate_for_keywords_of_document(document, threshold, top_n)
+        else:
+            raise ValueError(
+                f"Expected method to be either `doc_based` or `key_based`, but got {method}."
+            )
 
     @property
     def keywords(self) -> Tuple[str]:
@@ -91,6 +106,7 @@ class EmbeddingBasedComparator(BaseComparator):
         cache_keywords = list()
 
         if load_cache:
+            cache_dir = os.path.join(cache_dir, self.category.lower())
             if not os.path.exists(cache_dir):
                 raise ValueError(f"{cache_dir} is not found.")
             cache_keywords = [file.split(".")[0] for file in os.listdir(cache_dir)]
@@ -120,7 +136,7 @@ class EmbeddingBasedComparator(BaseComparator):
         document: str,
         threshold: Optional[float] = 0.6,  # -1.0 <= threshold <= 1.0
         top_n: Optional[int] = 5,
-    ):
+    ) -> Union[st.NewsCategory, float, List[str]]:
 
         """ Get Document Embedding """
         doc = Sentence(document)
@@ -153,8 +169,57 @@ class EmbeddingBasedComparator(BaseComparator):
 
         return news_category, score, most_likely_keywords
 
-    def _evaluate_for_keywords_of_document(self):
-        raise NotImplemented
+    def _evaluate_for_keywords_of_document(
+        self,
+        document: str,
+        threshold: Optional[float] = 0.6,  # -1.0 <= threshold <= 1.0
+        top_n: Optional[int] = 5,
+    ) -> Union[st.NewsCategory, float, List[str]]:
+
+        """ Get Keywords of Document as well as their embeddings """
+        keyextractor = keyExtractor(
+            tokenization_method_or_model="ckiptagger",
+            embedding_method_or_model="ckiplab/bert-base-chinese",
+        )
+        keywords_of_document = keyextractor.extract_keywords(
+            document, n_gram=2, top_n=5
+        )[0]
+
+        """ Score """
+        ## Get most likely keywords based on cosine similarity, threshold, and top_n
+        most_likely = list()
+        for keystruct in keywords_of_document:
+            for keyword, keyword_embedding in self._keywords_embeddings.items():
+                cs_score = ev.cosineSimilarity(keystruct.embedding, keyword_embedding)
+                if cs_score >= threshold:
+                    most_likely.append((keyword, cs_score))
+                    # logger.info(f"{keystruct.keyword} {keyword} {score}")
+
+        most_likely = sorted(most_likely, key=lambda k: k[1], reverse=True)
+        ## remove duplicate keywords
+        most_likely_remove_dup = list()
+        for (keyword, score) in most_likely:
+            if keyword not in [element[0] for element in most_likely_remove_dup]:
+                most_likely_remove_dup.append((keyword, score))
+
+        most_likely_remove_dup = most_likely_remove_dup[
+            : min(top_n, len(most_likely_remove_dup))
+        ]
+        most_likely_keywords = [element[0] for element in most_likely_remove_dup]
+
+        ## Get average score from most_likely
+        sum_score = sum([tmp[1] for tmp in most_likely])
+        score = round(sum_score / len(most_likely), 2) if most_likely else 0.0
+
+        """ Category """
+        news_category = st.NewsCategory.OTHER
+        if score > 0.50:
+            if self.category == st.NewsCategory.NN.value:
+                news_category = st.NewsCategory.NN
+            elif self.category == st.NewsCategory.ESG.value:
+                news_category = st.NewsCategory.ESG
+
+        return news_category, score, most_likely_keywords
 
 
 if __name__ == "__main__":
