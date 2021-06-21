a5a15d77e17e38436cd4abb1cea48c24175f28d7
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-06-21 15:50:59 +0800

    [commentout] embedding-related packages and functions

--- src/utils/struct.py
+++ src/utils/struct.py
@@ -3,25 +3,26 @@
 # Description: Data Structure
 
 import logging
-from typing import List, NamedTuple, Optional
-import torch
 from enum import Enum
+from typing import List, NamedTuple, Optional
+
+# import torch
 
 logger = logging.getLogger(__name__)
 
 
-class KeyStruct(NamedTuple):
+# class KeyStruct(NamedTuple):
 
-    id: int
-    keyword: List[str]
-    score: float
-    embedding: torch.tensor
+#     id: int
+#     keyword: List[str]
+#     score: float
+#     embedding: torch.tensor
 
-    def __eq__(self, other):
-        return self.keyword == other.keyword
+#     def __eq__(self, other):
+#         return self.keyword == other.keyword
 
-    def __repr__(self):
-        return f"({self.id}, {self.keyword}), {self.score}, {self.embedding.size()}"
+#     def __repr__(self):
+#         return f"({self.id}, {self.keyword}), {self.score}, {self.embedding.size()}"
 
 
 class NewsCategory(Enum):
